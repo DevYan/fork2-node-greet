98d88e3aa09c4db4e3a8afd0283a5a79add55486
DevYan <3027873@163.com>
2014-06-25 11:19:56 +0800

    warmup3 step1

--- bin/greet.js
+++ bin/greet.js
@@ -1,10 +1,3 @@
 #!/usr/bin/env node
-var greet=function greet(name,drunk){
-  if(drunk){
-    return "hello " + name + ",you look sexy today";
-  }else{
-    return "hello," + name;
-  }
-};
-var argv = require('minimist')((process.argv),{'--':true});
-console.log(greet(process.argv[2],argv.drunk));
+var greet=require("../lib/index.js");
+console.log(greet("zhangyan",true));
